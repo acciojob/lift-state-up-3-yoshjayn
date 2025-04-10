9d42d3d9fab3a3cf929552ce012cd4de82f72acb
Yashaswini <aswiniy68@gmail.com>
2025-04-10 17:17:20 +0530

    test-case

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,11 @@ import ChildComponent2 from './ChildComponent2';
 	const [selectedOption, setSelectedOption] = useState()
 
 	return (
-	  <div style={{backgroundColor:'#73FF00', width:'400px', padding:'20px'}}>
+	  <div className='parent' style={{backgroundColor:'#73FF00', width:'400px', padding:'20px'}}>
       <h1>Parent Component</h1>
 	  <ChildComponent1 prop={setSelectedOption}/>
 	  <ChildComponent2 prop={setSelectedOption}/>
-	  <p>Selected Option:{selectedOption}</p>
+	  <p>Selected Option: {selectedOption}</p>
 	  </div>
 	);
   }
